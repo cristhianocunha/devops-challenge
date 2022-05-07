3edacec347d3867fe3e1fdb3796a617ed481fd22
Cristhiano Cunha <cristhiano.cunha@gmail.com>
2022-05-06 21:43:55 -0300

    primeiro

---
+++ Grupo_de_Seguranca.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "apiki_grupoDeSeguranca" {
+  name = "apiki_grupoDeSeguranca"
+  ingress{
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+      from_port = 0
+      to_port = 0
+      protocol = "tcp"
+  }
+  ingress{
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+  }
+  ingress{
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+  }
+  ingress{
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+      from_port = 443
+      to_port = 443
+      protocol = "tcp"
+  }
+  egress{
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+  }
+  tags = {
+    Name = "apiki_grupoDeSeguranca"
+  }
+}
---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0cb4e786f15603b0d"
+  instance_type = "t2.micro"
+  key_name = "Apiki"
+  
+  tags = {
+    Name = "Apiki"
+  }
+}
+output "ip_publico" {
+  value       = aws_instance.app_server.public_ip
+  description = "ver ip publico"
+  
+}
+
+
+  
